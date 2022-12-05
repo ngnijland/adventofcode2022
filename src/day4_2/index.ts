c413e1375e2f0f9c1088fbdbcb0ff4ca98edf7c2
Niek Nijland <ngnijland@gmail.com>
2022-12-05 14:14:21 +0100

    feat: day4

---
+++ src/day4_2/index.ts
@@ -0,0 +1,50 @@
+import fs from "fs";
+import assert from "assert";
+import path from "path";
+
+function getOverlappingPairs(pairs: [string, string][]): number {
+  const overlappingPairs = pairs.filter(([sections1, sections2]) => {
+    const minmax1 = sections1.split("-").map(Number);
+    const minmax2 = sections2.split("-").map(Number);
+
+    // First section range overlaps second section range completely
+    if (minmax1[0] <= minmax2[1] && minmax1[1] >= minmax2[0]) {
+      return true;
+    }
+
+    // Second section range overlaps first section range completely
+    if (minmax2[1] <= minmax1[0] && minmax2[0] >= minmax1[1]) {
+      return true;
+    }
+
+    return false;
+  });
+
+  return overlappingPairs.length;
+}
+
+assert.strictEqual(
+  getOverlappingPairs([
+    ["2-4", "6-8"],
+    ["2-3", "4-5"],
+    ["5-7", "7-9"],
+    ["2-8", "3-7"],
+    ["6-6", "4-6"],
+    ["2-6", "4-8"],
+  ]),
+  4
+);
+
+fs.readFile(
+  path.join(__dirname, "..", "..", "src", "day4_2", "input"),
+  "utf8",
+  function (err: NodeJS.ErrnoException, data: string) {
+    if (err) throw err;
+
+    const input = data
+      .trim()
+      .split("\n")
+      .map((x) => x.split(","));
+    console.log(getOverlappingPairs(input as [string, string][]));
+  }
+);
